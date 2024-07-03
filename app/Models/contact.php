060c1174ce6eb26d4cf7aea842b25e4d8c1656f8
mannanshihab <amannanshihab@gmail.com>
2024-07-03 07:12:18 +0600

    update

--- app/Models/contact.php
+++ app/Models/contact.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class contact extends Model
+class Contact extends Model
 {
     use HasFactory, SoftDeletes;
 
